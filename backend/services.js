f6d615e0822091299a7827e8d0afa86625523d37
bawamohit <mohit.bawa@mail.utoronto.ca>
2022-03-02 23:37:01 -0500

    BIG CHANGES

---
+++ backend/services.js
@@ -0,0 +1,21 @@
+// Login
+
+function check_credentials(username, password) {
+    return true;
+}
+
+function signup(username, password) {
+    return true;
+}
+
+// Dashboard
+
+function get_flight_schedule(username) {
+    return JSON.stringify([]);
+}
+
+// Add Site
+
+// Add Flight
+
+// Logs
