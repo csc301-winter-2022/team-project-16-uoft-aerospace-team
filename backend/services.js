c4d0917a3c644520db2beedf205d555b6b846101
bawamohit <mohit.bawa@mail.utoronto.ca>
2022-03-03 00:27:58 -0500

    add services.js

--- backend/services.js
+++ backend/services.js
@@ -1,11 +1,18 @@
+// Message to backend team: import backend files necessary for the following function implementations
+
 // Login
 
-function check_credentials(username, password) {
+let user = null;
+
+function login(username, password) {
+    // check if username and password are correct
+    user = username;
     return true;
 }
 
 function signup(username, password) {
-    return true;
+    // make a new user and then
+    login(username)
 }
 
 // Dashboard
@@ -16,6 +23,46 @@ function get_flight_schedule(username) {
 
 // Add Site
 
+function get_airspace(pins) {
+    // pins is a list of coordinates
+    // loop through the pins and choose the most restrictive airspace
+
+    return 'G';
+}
+
+function get_nearby_aerodromes(pins) {
+    return JSON.stringify([]);
+}
+
+// ignore for now
+function get_geofence(pins) {
+    return JSON.stringify([]);
+}
+
+function create_site(username, name, pins, margin) {
+    // create the site
+}
+
 // Add Flight
 
+function get_sites(username) {
+    return JSON.stringify([]);
+}
+
+function get_site_aerodromes(sitename) {
+    return JSON.stringify([]);
+}
+
+function get_weather(date, sitename) {
+    return 'cloudy with a chance of balls';
+}
+
+function create_flight(date, sitename, pilot) {
+    // make the flight
+}
+
 // Logs
+
+function get_logs() {
+    return JSON.stringify([]);
+}
