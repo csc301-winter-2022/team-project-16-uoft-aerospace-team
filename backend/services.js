a5c2c1212645a3813e58488f5a17843ef3aabf71
danghoa3 <p.dang@mail.utoronto.ca>
2022-03-03 14:46:45 -0500

    fixed services.js

--- backend/services.js
+++ backend/services.js
@@ -2,67 +2,74 @@
 
 // Login
 
-let user = null;
+const UserUseCase = require("./User/UserUseCase.js");
+const userLogin = UserUseCase.login;
+const userSignup = UserUseCase.signup;
+
+// let user = null;
 
 function login(username, password) {
     // check if username and password are correct
-    user = username;
-    return true;
+    return userLogin(username, password);
 }
 
 function signup(username, password) {
-    // make a new user and then
-    login(username)
+    // make a new user and then log in
+    userSignup(username, password);
+    userLogin(username, password);
 }
 
-// Dashboard
+signup("peter", "dang");
+console.log(login("peter", "dang"));
 
-function get_flight_schedule(username) {
-    return JSON.stringify([]);
-}
+// // Dashboard
 
-// Add Site
+// function get_flight_schedule(username) {
+//     return JSON.stringify([]);
+// }
 
-function get_airspace(pins) {
-    // pins is a list of coordinates
-    // loop through the pins and choose the most restrictive airspace
+// // Add Site
 
-    return 'G';
-}
+// function get_airspace(pins) {
+//     // pins is a list of coordinates
+//     // loop through the pins and choose the most restrictive airspace
 
-function get_nearby_aerodromes(pins) {
-    return JSON.stringify([]);
-}
+//     return 'G';
+// }
 
-// ignore for now
-function get_geofence(pins) {
-    return JSON.stringify([]);
-}
+// function get_nearby_aerodromes(pins) {
+//     return JSON.stringify([]);
+// }
 
-function create_site(username, name, pins, margin) {
-    // create the site
-}
+// // ignore for now
+// function get_geofence(pins) {
+//     return JSON.stringify([]);
+// }
 
-// Add Flight
+// function create_site(username, name, pins, margin) {
+//     // create the site
+// }
 
-function get_sites(username) {
-    return JSON.stringify([]);
-}
+// // Add Flight
 
-function get_site_aerodromes(sitename) {
-    return JSON.stringify([]);
-}
+// function get_sites(username) {
+//     return JSON.stringify([]);
+// }
 
-function get_weather(date, sitename) {
-    return 'cloudy with a chance of balls';
-}
+// function get_site_aerodromes(sitename) {
+//     return JSON.stringify([]);
+// }
 
-function create_flight(date, sitename, pilot) {
-    // make the flight
-}
+// function get_weather(date, sitename) {
+//     return 'cloudy with a chance of balls';
+// }
 
-// Logs
+// function create_flight(date, sitename, pilot) {
+//     // make the flight
+// }
 
-function get_logs() {
-    return JSON.stringify([]);
-}
+// // Logs
+
+// function get_logs() {
+//     return JSON.stringify([]);
+// }
